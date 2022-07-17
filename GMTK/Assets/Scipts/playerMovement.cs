331dea9418e9f3e4d727d807353a9f9ce8c16348
Jonas Huang <89883151+jonaszchuang@users.noreply.github.com>
2022-07-16 20:09:53 -0600

    level 2

--- GMTK/Assets/Scipts/playerMovement.cs
+++ GMTK/Assets/Scipts/playerMovement.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class playerMovement : MonoBehaviour
 {
+    public string textValue;
+    public Text textElement;
     // Start is called before the first frame update
     void Start()
     {
