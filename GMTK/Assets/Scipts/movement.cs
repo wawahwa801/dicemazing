181dfc50687402eb863104d2dd919dcbcadda17c
Jonas Huang <89883151+jonaszchuang@users.noreply.github.com>
2022-07-16 18:21:10 -0600

    movement

--- GMTK/Assets/Scipts/movement.cs
+++ GMTK/Assets/Scipts/movement.cs
@@ -2,24 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerMovement : MonoBehaviour
+public class movement : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
         
     }
-    public float moveSpeed = 5f;
+    public float moveSpeed = 50f;
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetKeyDown(KeyCode.UpArrow))
             transform.Translate(Vector3.up * moveSpeed * Time.deltaTime);
         else if (Input.GetKey(KeyCode.DownArrow))
             transform.Translate(Vector3.down * moveSpeed * Time.deltaTime);
-        else if (Input.GetKey(KeyCode.RightArrow))
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
             transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
-        else if (Input.GetKey(KeyCode.LeftArrow))
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
             transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
     }
 }
--- GMTK/Assets/Scipts/playerMovement.cs
+++ GMTK/Assets/Scipts/playerMovement.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class movement : MonoBehaviour
+public class playerMovement : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
